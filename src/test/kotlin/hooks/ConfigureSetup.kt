986f99d9ab12d02242d44b299b1ce08bbf5f0591
laurentiu <laurentzyu_danyel@yahoo.com>
2022-04-05 22:38:23 +0300

    Api base URL added

--- src/test/kotlin/hooks/ConfigureSetup.kt
+++ src/test/kotlin/hooks/ConfigureSetup.kt
@@ -6,6 +6,7 @@ import utils.Settings
 object ConfigureSetup {
     fun initializeSettings() {
         Settings.baseUrl = Environment("baseUrl")
+        Settings.apiBaseUrl = Environment("apiBaseUrl")
         Settings.username = Environment("username")
         Settings.password = Environment("password")
         println("Settings configuration complete")
--- src/test/kotlin/utils/Settings.kt
+++ src/test/kotlin/utils/Settings.kt
@@ -3,6 +3,7 @@ package utils
 
 object Settings {
    lateinit var baseUrl: String
+   lateinit var apiBaseUrl: String
    lateinit var username: String
    lateinit var password: String
 }
